4ff15baae7b114750e2c10370b5e903025a19a5e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-19 23:09:34 -0300

    Exemplo api user Gitusers

--- pf0670/aula04/src/App.jsx
+++ pf0670/aula04/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Consumer from './components/consumer/Consumer'
+import Rodape from './components/rodape/Rodape'
 
 export default function App() {
 
     return (
         <>
             <h1>Exemplos de UseEffects</h1>
-            <Consumer/>
+            <Consumer />
+
         </>
     )
 }
