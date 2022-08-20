af9ffee47b106106e579f4e9c177fe8da1808b99
João <joao_olli@hotmail.com>
2022-08-19 23:02:54 -0300

    requisicao api (❁´◡`❁)

--- rm94231/aula04/src/components/consumer/Consumer.jsx
+++ rm94231/aula04/src/components/consumer/Consumer.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { useEffect } from 'react'
 export default function Consumer(){
     
+    const [usuarios, setUsuario] = useState([])
+
     useEffect(() => {
-        Document.title = "USE-EFFECTS"
+
+        async function carregaRepo() {
+            const response = await fetch("https://api.github.com/users")
+            const data = await response.json()
+
+            setUsuario(data)
+        }
+        carregaRepo()
     }, [])
 
-    return (
+    useEffect(() => {
+        document.title = "USE-EFFECTS ATUALIZOU"
+    }, [usuarios])
 
+    return (
         
-        <div>
+        <>
             <h1>Lista de Usuários do GitHub</h1>
-        </div>
+            <ul>
+                {usuarios.map((usuario) => 
+                    <li key={usuario.id}>
+                        <figure>
+                            <img src={usuario.avatar_url} alt={usuario.login} style={{width: '200px'}}></img>
+                            <figcaption><a href={usuario.html_url} target="_blank">{usuario.login}</a></figcaption>
+                        </figure>
+                    </li>
+                    
+                )}
+            </ul>
+        </>
     )
 }
