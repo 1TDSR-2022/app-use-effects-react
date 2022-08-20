63bf2a407f247a201003d4e38d5a8811654777a5
emersonnfs <rm95199@fiap.com.br>
2022-08-19 22:59:08 -0300

    Lista de usuarios criada

--- rm95199/aula04/aula04/src/components/consumer/Consumer.jsx
+++ rm95199/aula04/aula04/src/components/consumer/Consumer.jsx
@@ -3,15 +3,39 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Consumer() {
-    const [repositorio, setRepositorio] = useState();
+    const [usuarios, setUsuarios] = useState([])
     
     useEffect(() => {
-        document.title = "Presta atenção"
+
+      async function carregarUsuarios() {
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+        setUsuarios(data)
+        carregarUsuarios()
+      }
     }, [])
     
+    useEffect(() => {  
+      document.title = "USE-EFFECTS"
+    }, [usuarios])
+    
     return (
-    <div>
-      <h1>Lista de usuários do Github</h1>
-    </div>
-  )
+      <div>
+        <h1>Lista de usuários do Github</h1>
+        <ul>
+          {usuarios && usuarios.map(usuario => (
+            <li key={usuario.id}>
+              <figure>
+                <img src={usuario.avatar_url} alt={usuario.login} />
+                <figcaption>
+                  <a href={usuario.html_url}>
+                    {usuario.login}
+                  </a>
+                </figcaption>
+              </figure>
+              {usuario.login}</li>
+          ))}
+        </ul>
+      </div>
+    )
 }
