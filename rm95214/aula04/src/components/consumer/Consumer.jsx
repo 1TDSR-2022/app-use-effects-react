b91c24d185e6d5b51f814a9bd35f9ab026d48507
IgorEverton <rm95214@fiap.com.br>
2022-08-19 23:00:49 -0300

    Estraindo infos de .json

--- rm95214/aula04/src/components/consumer/Consumer.jsx
+++ rm95214/aula04/src/components/consumer/Consumer.jsx
@@ -4,20 +4,35 @@ import { useEffect } from "react"
 
 export default function Consumer(){
 
-    const [respositorio, setReporistorio] = useState()
+    const [usuarios, setUsuario] = useState([])
 
     useEffect(()=>{
-        document.title = 'USE-EFFECTS'
+        
+        async function carregaRepo(){
+            const response = await fetch("https://api.githutb.com/users")
+            const data = await response.json()
+
+            setUsuario(data)
+            
+        } 
+        carregaRepo()
+
     } ,[])
 
     useEffect(()=>{
         document.title = 'USE-STATE ATUALIZOU'
-    } ,[respositorio])
+    } ,[usuarios])
+
+
 
     return(
         <div>
             <h1>Lista de usuarios do Github</h1>
-            
+           <ul>
+            {usuarios.map((usuario, i)=>
+                <li key={i}>{usuario.login}</li>
+            )}
+           </ul>
         </div>
     )
 }
