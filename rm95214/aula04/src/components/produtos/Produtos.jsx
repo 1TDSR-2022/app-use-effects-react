579a847e3dbbee48cb273ecc0920fc6251ff3dda
IgorEverton <rm95214@fiap.com.br>
2022-08-24 22:51:26 -0300

    Aula sobre BrowserRouter

---
+++ rm95214/aula04/src/components/produtos/Produtos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Produtos(){
+    
+    const produtos={
+        backgroundColor : '#bff',
+        height : '85vh',
+        textAling: 'center',
+        color: '#7cc'
+    }
+
+    return(
+        <div style={produtos}>
+            <h1>Páginas de Produtos </h1>
+            <p>Exemplo de página principal de Produtos</p>
+        </div>
+    )
+}
---
+++ rm95214/aula04/src/components/servicos/Servicos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Servicos(){
+    
+    const servicos={
+        backgroundColor : '#bfb',
+        height : '85vh',
+        textAling: 'center',
+        color: '#7c7'
+    }
+
+    return(
+        <div style={servicos}>
+            <h1>Páginas de Servicços</h1>
+            <p>Exemplo de página dos Serviços</p>
+        </div>
+    )
+}
