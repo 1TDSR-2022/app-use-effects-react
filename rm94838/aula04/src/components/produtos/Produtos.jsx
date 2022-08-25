3445a3411f65038d9890475a653387674dc7207e
GustavoMeneguesso <rm94838@fiap.com.br>
2022-08-24 23:07:26 -0300

    .

---
+++ rm94838/aula04/src/components/produtos/Produtos.jsx
@@ -0,0 +1,16 @@
+export default function Produtos(){
+
+    const produto = {
+        backgroundColor: '#bff',
+        height: '85vh',
+        textAlign: 'center',
+        color: '#7cc' 
+    }
+
+    return(
+        <div style={produto}>
+            <h1>Página de produtos</h1>
+            <p>Exemplo da página de produtos de compra</p>
+        </div>
+    )
+}
---
+++ rm94838/aula04/src/components/servicos/Servicos.jsx
@@ -0,0 +1,15 @@
+export default function Servicos(){
+    const servico = {
+        backgroundColor: '#bfb',
+        height: '85vh',
+        textAlign: 'center',
+        color: '#7c7'
+    }
+
+    return(
+        <div style={servico}>
+            <h1>Página de Seriços</h1>
+            <p>Exemplo da página dos Serviços</p>
+        </div>
+    )
+}
