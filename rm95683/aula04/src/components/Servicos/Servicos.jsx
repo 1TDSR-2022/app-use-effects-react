8cb4c6a1e3111fe30d98e131cfbc6a15fdb8a52e
IagoCoelho <rm95683@fiap.com.br>
2022-08-24 22:45:38 -0300

    routes.

---
+++ rm95683/aula04/src/components/Servicos/Servicos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Servicos(){
+    const servico = {
+        backgroundColor: '#bfb',
+        height: '85vh',
+        textAlign: 'center',
+        color: '#7c7'
+    }
+
+    return(
+        <div style={servico}>
+            <h1>Página de Serviços</h1>
+            <p>Exemplo de página dos Serviços.</p>
+        </div>
+    )
+}
