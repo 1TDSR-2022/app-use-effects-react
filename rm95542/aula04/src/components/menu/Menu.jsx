e704bf0159ac74b3d1539847f8cc50508679c092
guilherme90rr <rm95542@fiap.com.br>
2022-08-26 16:28:36 -0300

    Tudo_ok.

--- rm95542/aula04/src/components/menu/Menu.jsx
+++ rm95542/aula04/src/components/menu/Menu.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import{Link} from 'react-router-dom'
+import './menu.css'
+
 export default function Menu() {
   return (
-
     <header>
         <nav>
-            <ul>
-                <BrowserRouter>
-            
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/produtos">Produtos</Link></li>
-                    <li><Link to="/servicos">Servicos</Link></li>
-                </BrowserRouter>
+            <ul>          
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/produtos">Produtos</Link></li>
+              <li><Link to="/servicos">Servicos</Link></li>
             </ul>
         </nav>
     </header>
