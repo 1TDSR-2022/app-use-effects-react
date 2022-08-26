458c079fe859b17940956cc8673b22c9d87032dd
lucasmoraisnt <RM93267@fiap.com.br>
2022-08-26 19:42:34 -0300

    Acrescentado história e curiosidade em "sobre" da loja Renner

--- rm93267/aula04/src/components/sobre/Sobre.jsx
+++ rm93267/aula04/src/components/sobre/Sobre.jsx
@@ -3,14 +3,20 @@ import React from 'react'
 export default function Sobre() {
   const corpo={
     paddingLeft: '10px',
-    borderRadius: '5px',
-    marginRight: '80%',
-    marginBottom: '20%'
+    marginRight: '50%',
+    marginBottom: '12%'
    
   }
 
   return (
     <div style={corpo}>
+      <div>
+        <h2>História da loja-moda Renner</h2>
+        <p>A Lojas Renner S.A. teve seu início em 1922, com o começo das atividades fabris do então Grupo A. J. Renner, e desvinculou-se do grupo fundado pelo gaúcho Antônio Jacob 
+          Renner somente em 1965, quando suas lojas começaram a tomar um formato mais próximo do atual.</p>
+        <p>Por meio das marcas próprias, a Lojas Renner oferece produtos voltados para diversos estilos e tendências da moda, separados por variados setores, como bazar, banho, 
+          cama, artigos para presente, móveis, artigos esportivos, etc.</p>
+      </div>
         <h1>Loja-Moda feita por</h1>
         <p>by: &copy; Lucas de Morais Nascimento Taguchi </p>
         <p>RM93267</p>
