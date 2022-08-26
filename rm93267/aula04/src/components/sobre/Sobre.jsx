3ffcec26fa38e5cb8286a5b49c00f77527fc778b
lucasmoraisnt <RM93267@fiap.com.br>
2022-08-25 21:39:01 -0300

    Copyright

--- rm93267/aula04/src/components/sobre/Sobre.jsx
+++ rm93267/aula04/src/components/sobre/Sobre.jsx
@@ -4,7 +4,7 @@ export default function Sobre() {
   return (
     <>
         <h1>Loja-Moda feita por</h1>
-        <p>by: Lucas de Morais Nascimento Taguchi </p>
+        <p>by: &copy; Lucas de Morais Nascimento Taguchi </p>
         <p>RM93267</p>
     </>
   )
