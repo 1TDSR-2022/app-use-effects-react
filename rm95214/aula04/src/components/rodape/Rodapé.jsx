66ebfb3c25a26d049a1045ea33434990b84a0a82
IgorEverton <rm95214@fiap.com.br>
2022-08-27 19:03:08 -0300

    construindo o componente rodape

---
+++ rm95214/aula04/src/components/rodape/Rodapé.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Rodape(){
+    
+    const footer={
+        position: 'absolute',
+        bottom: '0',
+        backgroundColor: 'blue',
+        color: '#FFF',
+        width: '100%',
+        height: '100px ',
+    }
+    
+    return(
+
+    )
+}
